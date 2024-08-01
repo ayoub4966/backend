a049a61491a77464babd5565c43f916dbe504ae7
ayoub4966 <tayebayoub33@gmail.com>
2024-08-01 20:16:31 +0100

    first commit

---
+++ api/src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { jwtConstants } from './constants';
+import { JwtModule } from '@nestjs/jwt';
+import { UsermanagmentModule } from 'src/usermanagment/usermanagment.module';
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService],
+  imports: [
+    UsermanagmentModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  exports: [AuthService],
+})
+export class AuthModule {}
